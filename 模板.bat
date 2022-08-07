61287ddefda39ff9b9e3e3ad948de4a3800c97f4
夜看星空 <46042287+Yekanxingkong@users.noreply.github.com>
2022-08-07 16:13:26 +0800

    2022年8月7日

---
+++ 模板.bat
@@ -0,0 +1,18 @@
+start
+
+echo f=shift delete /s=del all flie /q=quiet del.
+
+>NUL 2>&1 REG.exe query "HKU\S-1-5-19" || (
+    ECHO SET UAC = CreateObject^("Shell.Application"^) > "%TEMP%\GetAdmin.vbs"
+    ECHO UAC.ShellExecute "%~f0", "%1", "", "runas", 1 >> "%TEMP%\GetAdmin.vbs"
+    "%TEMP%\GetAdmin.vbs"
+    DEL /f /q "%TEMP%\GetAdmin.vbs" 2>NUL
+    Exit /b
+)
+
+
+
+
+taskkill /f /im cmd.exe
+
+exit
