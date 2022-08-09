eff9357536b9a6df060dbfe5ff156b2a5599aeee
夜看星空 <46042287+Yekanxingkong@users.noreply.github.com>
2022-08-10 00:14:18 +0800

    2022年8月10日

---
+++ TCP客户端/Project1/main.cpp
@@ -0,0 +1,7 @@
+#include <pah.h>
+using namespace std;
+int main()
+{
+	TCP_client();
+	return 0;
+}
---
+++ TCP服务端/Project2/pah.h
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef PAH_H
+#define PAH_H
+
+//你的代码写在这里
+int JTCPserver();
+
+#endif
